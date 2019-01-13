b6aa9d0dbf6abe5953bf7aa69a6d5ae4d5165da5
Alyona Andr <alyona.andriyevskaya@gmail.com>
2019-01-13 16:26:20 +0200

    install fontawesome and create Task__Element

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import './style.css';
 
 class Dashboard extends Component {
 	render() {
 		return(
-			<div>Dashboard</div>
+			<div className="row my-5">
+				<div className="container">
+					<div className="row flex-column align-items-center">
+						<div className="task__element col-12 col-md-8 col-lg-7 d-flex">
+							<div className=" col-2 task__title">
+								task__title
+							</div>
+							<div className="col-8 task__desc">
+								task__desc
+							</div>
+							<div className="col-2 task__controls">
+								<button>Done</button>
+								<button>Remove</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		);
 	}
 	
